702cc3aa1ac73955ad733724635b85c77146e9cf
Daniel Faller Grass <daniel-faller-grass@hotmail.com>
2021-11-07 17:39:33 -0300

    Aulas de Ruby Puro

---
+++ Aula_2/onebit_io.rb
@@ -0,0 +1,18 @@
+# Saída de dado
+print 'Digite seu nome: '
+
+# Recebendo uma entrada do teclado
+nome = gets.chomp
+
+# Saída de dado
+print 'Digite seu sobrenome: '
+
+#Recebendo uma entrada do teclado
+sobrenome = gets.chomp
+
+# Saída  utilizando puts
+# Use o código ruby dentro de uma string com #{code}
+puts "hello #{nome} #{sobrenome}!"
+
+#Alternativa de saída de dado
+p 'Que teste legal!'
