7b518a0f2661b23667cee84322fb4f058bf291b5
Daniel Grass <daniel-faller-grass@hotmail.com>
2021-11-11 23:26:53 -0300

    iteraçao e condicionais

---
+++ Aula_3/iteracao_ou_repeticoes.rb
@@ -0,0 +1,28 @@
+# FOR
+fruits = ['banana', 'uva', 'maça']
+
+for fruit in fruits
+    puts "frutas da cesta: #{fruit}"
+end
+
+#WHILE
+x = 1
+
+while x <= 10
+    puts x
+    x += 1 
+end
+
+#LOOP
+count = 1
+loop do
+    puts count
+    break if count == 20
+    count += 1
+end
+
+#TIMES
+y = 0
+30.times do
+    puts y += 1
+end
