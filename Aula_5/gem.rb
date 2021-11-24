bb83b52e2f5db6c0de3eed726992681550c7db33
Daniel Faller Grass <daniel-faller-grass@hotmail.com>
2021-11-24 00:22:54 -0300

    Aula 5 Metodos e Gems

---
+++ Aula_5/gem.rb
@@ -0,0 +1,15 @@
+require 'os' #informa que a gem deve ser instalada antes de rodar
+
+def my_os
+    if OS.windows?
+        'Windows'
+    elsif OS.linux?
+        'Linux'
+    elsif OS.mac?
+        'Osx'
+    else
+        'Não consegui identificar'
+    end
+end
+
+puts "Meu PC possui #{OS.cpu_count} cores, é #{OS.bits} bits e o sistema operacional é #{my_os}"
