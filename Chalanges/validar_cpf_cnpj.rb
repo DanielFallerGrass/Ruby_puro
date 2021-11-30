7c53ccf02cb595b4e96bb0823bfadc3a71c506ce
Daniel Faller Grass <daniel-faller-grass@hotmail.com>
2021-11-29 23:54:37 -0300

    Ajuste função validador CPF/CNPJ

--- Chalanges/validar_cpf_cnpj.rb
+++ Chalanges/validar_cpf_cnpj.rb
@@ -1,16 +1,16 @@
 require 'cpf_cnpj'
 
+def validar
+    if CPF.valid?(@cpf_cnpj)
+        puts "É um número de CPF válido. (#{@cpf_cnpj})"
+    else
+        puts "É um número de CPF inválido. (#{@cpf_cnpj})"
+    end        
+end
+
 puts 'informe um CPF ou CNPJ sem pontuação a ser validado:'
 @cpf_cnpj = gets.chomp
 
-def validar
-    @result = CPF.valid?(@cpf_cnpj)
-end
+resultado = validar
 
-validar
-
-if @result == true
-    puts "É um número de CPF válido. (#{@cpf_cnpj})"
-else
-    puts "É um número de CPF inválido. (#{@cpf_cnpj})"
-end
+puts resultado
