c18d6d03b36c3820683cc8b7d5c687ab61e827f4
Daniel Faller Grass <daniel-faller-grass@hotmail.com>
2021-12-02 00:36:44 -0300

    Ajuste comments

--- Chalanges/validar_cpf_cnpj.rb
+++ Chalanges/validar_cpf_cnpj.rb
@@ -3,13 +3,13 @@ require 'cpf_cnpj'
 
 def validar
     if CPF.valid?(@cpf_cnpj)
-        puts "É um número CPF ou CNPJ válido. (#{@cpf_cnpj})"
+        puts "É um número de CPF ou CNPJ válido. (#{@cpf_cnpj})"
     else
-        puts "É um número CPF ou CNPJ inválido. (#{@cpf_cnpj})"
+        puts "É um número de CPF ou CNPJ inválido. (#{@cpf_cnpj})"
     end        
 end
 
-puts 'informe um CPF ou CNPJ sem pontuação a ser validado:'
+puts 'informe um CPF ou CNPJ sem ou com pontuação a ser validado:'
 @cpf_cnpj = gets.chomp
 
 resultado = validar
