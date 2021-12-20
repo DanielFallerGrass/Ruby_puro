8f61cb9bf2d5af061686dcb66adbbaa701d689ac
Daniel Faller Grass <daniel-faller-grass@hotmail.com>
2021-12-20 00:50:07 -0300

    Aula_8 (Continuar)

---
+++ Challenges/third_project/projeto.rb
@@ -0,0 +1,23 @@
+=begin
+O ruby oferece um método chamado ```capitalize``` para tornar a primeira letra de uma string maiúscula.
+
+Sabendo disso crie uma lambda que recebe um nome como parâmetro e o imprime com a primeira letra maiúscula.
+Esta lambda deverá ser salva dentro de uma variável que será passada como argumento de um método chamado capitalize_name.
+
+Dentro deste método você chamará a lambda duas vezes, passando como parâmetro em cada uma delas um nome diferente.
+
+
+capitalize_name = -> (nomes) {nomes.each {|name | puts name.capitalize}}
+
+nomes = ["daniel", "raquel"]
+
+capitalize_name.call(nomes)
+=end
+
+texto = -> {puts texto.capitalize}
+
+def capitalize_name
+    nomes =
+  
+end
+
