fc5feffa58986ee6e3bb6d8e5d326eec89cf21c0
Daniel Faller Grass <daniel-faller-grass@hotmail.com>
2021-12-19 23:19:54 -0300

    Ajustes lambda

--- Aula_8/Lambda/lambda.rb
+++ Aula_8/Lambda/lambda.rb
@@ -4,15 +4,15 @@ first_lambda.call
 first_lambda.call
 
 # 2 - Você pode abreviar a declaração de uma lambda da seguinte forma "->"
-first_lambda = -> { puts "my first lambda" }
-first_lambda.call
+second_lambda = -> { puts "my first lambda" }
+second_lambda.call
 
 # 3 - Uma lambda também pode receber parâmetros para sua execução (Perceba que você executou um block dentro de uma lambda)
-firt_lambda = -> (nomes) {names.each { |name |puts name} }
+third_lambda = -> (nomes) {nomes.each { |name |puts name} }
 
 nomes = ["joão", "maria", "pedro"]
 
-first_lambda.call(nomes)
+third_lambda.call(nomes)
 
 # 4 - Em lambdas que ocupam várias linhas, não declare a lambda de forma abreviada e utilize o do.end
 my_lambda = lambda do |numbers|
