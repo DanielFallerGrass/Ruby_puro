2092e9eaa1cabf00303aea7e973ab8178b1d30b3
Daniel Faller Grass <daniel-faller-grass@hotmail.com>
2022-01-03 17:22:03 -0300

    Aula_9(challenges: projeto1)

--- Challenges/regex_validations_BR_phone.rb
+++ Challenges/regex_validations_BR_phone.rb
@@ -1,3 +1,3 @@
 p "(99) 7 4321-1234".match(/[(]\d{2}[)]\s\d{1}\s\d{4}[-]\d{4}/) #O <\s>  Seria equivalente ao space
 
-p "(99) 7 4321-1234".strip.delete(" ").match(/[(]\d{2}[)]\d{1}\d{4}[-]\d{4}/) #O <.delete(" ") >  Seria serve nesse caso para remover qualquer espaço em branco como se fosse um <trim> ou <strip>
+p "(99) 7 4321-1234".delete(" ").match(/[(]\d{2}[)]\d{1}\d{4}[-]\d{4}/) #O <.delete(" ") >  Seria serve nesse caso para remover qualquer espaço em branco como se fosse um <trim> ou <strip>
